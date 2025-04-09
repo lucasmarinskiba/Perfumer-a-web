13c7289082ba42cf7ea5d2fd3beaf81da1b88568
Lucas Daniel Marín <116664832+lucasmarinskiba@users.noreply.github.com>
2025-04-09 18:04:35 -0300

    Delete bash.sh

--- bash.sh
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/tu-usuario/tu-usuario.github.io.git
-cd tu-usuario.github.io
-# Copia aquí todos tus archivos del proyecto
-git add .
-git commit -m "Primer commit del sitio de perfumería"
-git push
