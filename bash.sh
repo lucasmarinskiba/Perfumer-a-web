51191b91fbc9edbe4e18b9dd12cd2189ef06cea4
Lucas Daniel Marín <116664832+lucasmarinskiba@users.noreply.github.com>
2025-04-09 18:02:31 -0300

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/tu-usuario/tu-usuario.github.io.git
+cd tu-usuario.github.io
+# Copia aquí todos tus archivos del proyecto
+git add .
+git commit -m "Primer commit del sitio de perfumería"
+git push
