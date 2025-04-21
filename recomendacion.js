29d4af249b686b23c54ede273057c099e2aec0f5
Lucas Daniel Marín <116664832+lucasmarinskiba@users.noreply.github.com>
2025-04-21 19:25:45 -0300

    Update recomendacion.js

--- recomendacion.js
+++ recomendacion.js
@@ -2,3 +2,5 @@ function recomendarPerfume() {
     // Lógica basada en preferencias (floral, amaderado, cítrico)
     // Devuelve 3 opciones personalizadas
 }
+//realidad aumentada
+<script src="https://aframe.io/releases/1.2.0/aframe.min.js"></script>
